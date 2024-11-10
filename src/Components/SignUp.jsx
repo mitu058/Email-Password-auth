9fdfa7d9fcbe7dc049c3a0bcccd6115940939312
mitu058 <mitum9796@gmail.com>
2024-11-10 12:55:28 +0600

    toggle show password and accept terms

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,28 +1,39 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import auth from "../firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const handelSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
+    console.log(email, password,terms);
     // rest error message
     setErrorMessage("");
     setSuccess(false);
+      if(!terms){
+        setErrorMessage('please accept our terms and conditions')
+        return
+      }
+
     if (password.length < 6) {
       setErrorMessage("password must be at least 6 charecter or longer");
       return;
     }
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-    if(!passwordRegex.test(password)){
-        setErrorMessage('At least one upperCase, one lowerCase, one number, one special charecter')
-        return;
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "At least one upperCase, one lowerCase, one number, one special charecter"
+      );
+      return;
     }
-    
+
     // creat user with email and password
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
@@ -54,33 +65,48 @@ const SignUp = () => {
                 required
               />
             </div>
-            <div className="form-control">
+            <div className="form-control relative">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
+              <button
+                onClick={() => setShowPassword(!showPassword)}
+                className="btn btn-xs absolute right-4 top-12"
+              >
+                {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+              </button>
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
               </label>
             </div>
+            {/* terms and consition */}
+            <div className="form-control">
+              <label className="label cursor-pointer justify-start">
+              <input type="checkbox" name="terms" className="checkbox  checkbox-sm" />
+                <span className="label-text ml-4">Accept our terms and conditions</span>
+              </label>
+            </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary">Sign Up</button>
             </div>
           </form>
-          {
-          errorMessage && <p className="text-center pb-3 text-red-500">{errorMessage}</p>
-          }
-          {
-            success && <p className="text-green-600 text-center pb-3 text-lg">Successfully Sign Up</p>
-          }
+          {errorMessage && (
+            <p className="text-center pb-3 text-red-500">{errorMessage}</p>
+          )}
+          {success && (
+            <p className="text-green-600 text-center pb-3 text-lg">
+              Successfully Sign Up
+            </p>
+          )}
         </div>
       </div>
     </div>
